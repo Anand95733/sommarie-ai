63b89e28a6188b39754abfec3d72253778ad0426
Anand <goudanand19@gmail.com>
2026-04-10 18:35:25 +0530

    fix the deploy error

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,2 +1,2 @@
 export const isDev = process.env.NODE_ENV === "development";
-export const ORIGIN_URL = isDev ? 'http://localhost:3000' : ''
+export const ORIGIN_URL = isDev ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_APP_URL || 'https://your-app.vercel.app'
